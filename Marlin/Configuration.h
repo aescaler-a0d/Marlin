88b1d585fca3e3ac5d8312c76a5c464ad9c2b991
AJ Escalera <aj@aod.sh>
2022-04-02 15:25:53 -0400

    changed author string

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(angelofdeauth, CR-10_SKR1.4T)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
